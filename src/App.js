bd7678adae63b55353ced8989fbf3f879e1735f8
Joese <fransasisi7@gmail.com>
2022-06-01 22:11:50 +0700

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-
-
-
 function App() {
   return (
     <div className="App">
       <h1 className="text-6xl font-bold underline">Hello world!</h1>
+      <h2>Belajar menggunakan Tailwind</h2>
     </div>
   );
 }
