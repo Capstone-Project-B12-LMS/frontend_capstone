cfb7615f85c6320534d649d79311163351e25b68
inganta23 <johanes230500@gmail.com>
2022-06-02 11:44:32 +0700

    Adding Login Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement, incrementByAmount } from "./redux/contohSlice";
+import Login from "./components/Login";
+
 
 function App() {
-  const dispatch = useDispatch();
-  const { value } = useSelector((state) => state.contoh);
+ 
   return (
-    <div className="App">
-      <h1 className="text-6xl mb-4 font-bold underline">Hello world!</h1>
-      <p className="mb-2">Value : {value}</p>
-      <button className="p-2" onClick={() => dispatch(increment())}>
-        Increment
-      </button>
-      <button className="p-2 mx-2" onClick={() => dispatch(decrement())}>
-        Decrement
-      </button>
-      <button className="p-2" onClick={() => dispatch(incrementByAmount(2))}>
-        Increment By Amount
-      </button>
+    <div>
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+        <div className="w-[1000px] h-[800px] flex flex-col items-center justify-center bg-[#fff] rounded-[30px]">
+          <h2 className="text-[32px] leading-[48px] mb-[7px]">Login</h2>
+          <h4 className="text-2xl font-medium mb-10">Please login here</h4>
+          <form className="w-[640px] flex flex-col items-center">
+            <div className="w-full mb-8">
+              <label htmlFor="username" className="text-2xl leading-9 mb-2">
+                Username
+              </label>
+              <input
+                className="border-[1px] rounded-[10px] border-[#D9D9D9] focus:outline-none w-full h-[62px] placeholder:text-[20px] placeholder:leading-[30px]"
+                placeholder="Username"
+                type="text"
+                name="username"
+                required
+              />
+            </div>
+            <div className="w-full mb-10">
+              <label htmlFor="password" className="text-2xl leading-9 mb-2">
+                Password
+              </label>
+              <input
+                className="border-[1px] rounded-[10px] border-[#D9D9D9] focus:outline-none w-full h-[62px] placeholder:text-[20px] placeholder:leading-[30px]"
+                placeholder="Password"
+                type="text"
+                name="password"
+                required
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full h-[62px] rounded-[20px] text-xl leading-[30px] font-medium mb-5"
+            >
+              Login
+            </button>
+            <p className="text-2xl leading-9 text-[#ADADAD]">
+              Don't have an account?{" "}
+              <span className="text-[#000] font-semibold">Register</span>
+            </p>
+          </form>
+        </div>
+    </div>
+  );
+};
+
+export default Login;
