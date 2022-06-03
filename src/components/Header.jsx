8fdcfed05d6e9649a9b1a39aa3e480a0319c4399
Joese <fransasisi7@gmail.com>
2022-06-03 10:35:19 +0700

    home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <div className="absolute w-[1512px] h-[120px] bg-[#D9D9D9]">
+      <div className="relative w-[1365px] h-[100px] bg-[#A5CECD]">
         <div className="grid grid-cols-3 ">
-          <div className="absolute w-[80px] h-[80px] ml-[64px] mt-[20px] bg-[#000000]"></div>
+          <div className="absolute w-[80px] h-[80px] ml-[64px] mt-[15px] bg-[#000000]"></div>
           <button
             type="submit"
-            className="w-full h-[52px] w-[113px] ml-[1078px] mt-[34px] rounded-[10px] text-xl leading-[30px] font-medium mb-5"
+            className=" h-[52px] w-[113px] ml-[1028px] mt-[30px] rounded-[10px] text-xl font-medium mb-5 bg-[#FFFFFF] text-[#A5CECD]"
           >
             Login{" "}
           </button>
           <button
             type="submit"
-            className="w-full h-[52px] w-[117px] ml-[710px] mt-[34px] rounded-[10px] text-xl leading-[30px] font-medium mb-5"
+            className="w-full h-[52px] w-[117px] ml-[710px] mt-[30px] rounded-[10px] text-xl leading-[30px] font-medium mb-5 bg-transparent outline outline-2"
           >
             Sign up{" "}
           </button>
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,9 +1,58 @@
 import Header from "../components/Header";
+import home from "../assets/img/home.png";
 
 const Index = () => {
   return (
     <div>
       <Header />
+      <body>
+        <div className="container mx-auto px-6">
+          <div className="flex">
+            <div className="flex-1">
+              {" "}
+              <h2>Welcome To </h2>
+              <h2>The world of Learning</h2>
+              <div className="mt-[20px] text-[25px]">
+                <strong>The knowledge you gain quickly and efficiently</strong>
+              </div>
+              <div className="flex-1">
+                <img src={home} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="container mx-auto">
+          <div className="static w-[1112px] h-[862-px] mt-[100]">
+            <div className="static w-[687px] h-[491px] ml-[610px] mt-[12px]">
+              <img src={home} alt="" />
+              <div className="static">
+                <h2>Welcome To </h2>
+                <h2>The world of Learning</h2>
+                <div className="mt-[20px] text-[25px]">
+                  <strong>
+                    The knowledge you gain quickly and efficiently
+                  </strong>
+                </div>
+              </div>
+            </div>
+
+            <div className="relative w-[1112px] h-[862-px] mt-[120]">
+              <div className="relative w-[687px] h-[491px] ml-[610px] mt-[172px]">
+                <img src={home} alt="" />
+              </div>
+              <div className="absolute mt-[320px] ">
+                <h2>Welcome To </h2>
+                <h2>The world of Learning</h2>
+                <div className="mt-[20px] text-[25px]">
+                  <strong>
+                    The knowledge you gain quickly and efficiently
+                  </strong>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> */}
+      </body>
     </div>
   );
 };
