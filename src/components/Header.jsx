919a1ddbbe15ce92b9751d7731bbd200d56e68ef
Joese <fransasisi7@gmail.com>
2022-06-04 18:36:37 +0700

    add footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,21 +4,27 @@ const Header = () => {
   return (
     <header>
       <div>
-        <div className="flex">
-          <div className="w-full bg-transparent grid grid-cols-3 ">
-            <div className="absolute rounded-full w-[60px] h-[60px] ml-[64px] mt-[15px] bg-[#000000]"></div>
-            <button
-              type="submit"
-              className=" h-[52px] w-[113px] ml-[1028px] mt-[20px] rounded-[10px] text-xl font-medium mb-5 bg-[FF997A] "
-            >
-              Login{" "}
-            </button>
-            <button
-              type="submit"
-              className="w-full h-[52px] w-[117px] ml-[710px] mt-[20px] rounded-[10px] text-xl leading-[30px] font-medium mb-5 bg-transparent outline outline-2 outline-[#415A80] text-[#415A80]"
-            >
-              Sign up{" "}
-            </button>
+        <div className="relative">
+          <div className="flex w-full bg-transparent grid grid-cols-3 ">
+            <div>
+              <div className="absolute rounded-full w-[80px] h-[80px] ml-[80px] mt-[15px] bg-[#000000]"></div>
+            </div>
+
+            <div></div>
+            <div>
+              <button
+                type="submit"
+                className="w-full h-[52px] w-[113px] ml-[100px] mt-[20px] rounded-[10px] text-xl leading-[30px] font-medium mb-5 "
+              >
+                Login
+              </button>
+              <button
+                type="submit"
+                className="w-full h-[52px] w-[117px] ml-[50px] mt-[20px] rounded-[10px] text-xl leading-[30px] font-medium mb-5 bg-transparent outline outline-2 outline-[#415A80] text-[#415A80]  "
+              >
+                Sign up{" "}
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -28,21 +28,21 @@ const Index = () => {
               <div className="">
                 <div className="grid justify-items-center">
                   <div className=" grid grid-cols-3 w-[1100px] h-[654px]">
-                    <div className="box-border border-2 bg-[#C0DEDD] rounded-l-xl"></div>
-                    <div className="col-span-2 box-border border-2 rounded-r-xl ">
+                    <div className=" bg-[#C0DEDD] rounded-l-xl shadow-[0px_2px_0px_rgba(0,0,0,1)]"></div>
+                    <div className="col-span-2  rounded-r-xl shadow-[1px_2px_2px_rgba(0,0,0,1)]">
                       <h2 className="text-center mt-4">Contact US</h2>
                       <br></br>
                       <div className="grid place-items-center mx-auto px-5 ">
                         <form>
-                          <label for="email-address-icon">Your Name</label>
-                          <div class="relative">
-                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                          <label>Your Name</label>
+                          <div className="relative">
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="16"
                                 height="16"
                                 fill="currentColor"
-                                class="bi bi-person"
+                                className="bi bi-person"
                                 viewBox="0 0 16 16"
                               >
                                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
@@ -50,15 +50,16 @@ const Index = () => {
                             </div>
                             <input
                               type="text"
-                              id="email-address-icon"
-                              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              id="name-address-icon"
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                               placeholder="Full Name"
+                              name="name"
                             />
                           </div>
                           <br></br>
-                          <label for="email-address-icon">Your Email</label>
-                          <div class="relative">
-                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                          <label>Your Email</label>
+                          <div className="relative">
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="16"
@@ -73,20 +74,21 @@ const Index = () => {
                             <input
                               type="text"
                               id="email-address-icon"
-                              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                               placeholder="example@gmail.com"
+                              name="email"
                             />
                           </div>
                           <br></br>
-                          <label for="email-address-icon">Your Message</label>
-                          <div class="relative">
-                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                          <label>Your Message</label>
+                          <div className="relative">
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="16"
                                 height="16"
                                 fill="currentColor"
-                                class="bi bi-text-paragraph"
+                                className="bi bi-text-paragraph"
                                 viewBox="0 0 16 16"
                               >
                                 <path
@@ -97,17 +99,18 @@ const Index = () => {
                             </div>
                             <textarea
                               type="text"
-                              id="email-address-icon"
-                              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              id="message-address-icon"
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                               placeholder="Message"
                               rows="8"
                               cols="80"
+                              name="message"
                             />
                           </div>
                           <br></br>
                           <button
                             type="submit"
-                            className="w-[660px] h-[52px] rounded-[10px] text-xl font-medium mb-5"
+                            className="w-[660px] h-[52px] rounded-[10px] text-xl font-medium mb-5  hover:bg-[#839dc3]"
                           >
                             Submit
                           </button>
