cd961bc983da44f6fc12d044b8190afe22f3a0f3
FerriRama Chandra <FerriRamaChandra@gmail.com>
2022-06-08 15:55:10 +0800

    Add link

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,11 @@
-import "./Card.css"
 import pic from "../../images/Dragunov.jpg"
+
+
 const Card = ({ CardTitle, CardProgress }) => {
     return (
         <div className="mt-8">
             <div className="bg-white rounded overflow-hidden shadow-md w-[300px] h-[240px]">
-                <img src={pic} alt="Picture" className="CardImage" />
+                <img src={pic} alt="Picture" className="w-[100%] h-[150px]" />
                 <div className="m-4">
                     <span className="font-bold">{CardTitle}</span>
                     <p className="block text-gray-500 text-sm">Progress: {CardProgress}</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Card from "../components/Card";
 import "./style.css"
+import { Link } from "react-router-dom";
 
 
 const Home = () => {
@@ -11,8 +12,8 @@ const Home = () => {
                         <h2>Welcome back Veronica !</h2>
                         <p>Do you want to add a new class?</p>
                         <div className="container_button">
-                            <button className="w-[35%] h-[30px] button_join">Join Class</button>
-                            <button className="w-[35%] h-[30px] button_class">Create Class</button>
+                            <button className="w-[35%] h-[30px] button_join"><Link to="/dashboard/join">Join Class</Link></button>
+                            <button className="w-[35%] h-[30px] button_class"><Link to="/dashboard/create-class">Create Class</Link></button>
                         </div>
                     </div>
                     <div className="content">
@@ -23,7 +24,7 @@ const Home = () => {
                 <div className="w-[90%]">
                     <div className="card_text">
                         <h3>Active course</h3>
-                        <h3>View All</h3>
+                        <h3><Link to="/class">View All</Link></h3>
                     </div>
                     <div className="container_course">
                         <div className="course w-[90%]" >
