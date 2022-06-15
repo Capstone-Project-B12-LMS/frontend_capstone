68a2fca2b0db38eac0603c673f914b114a53808e
inganta23 <johanes230500@gmail.com>
2022-06-15 22:18:37 +0700

    Add Update Account Data

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -8,21 +8,29 @@ import AddIcon from "../assets/icons/add-icon.svg";
 import CreateIcon from "../assets/icons/create-class-icon.svg";
 import JoinIcon from "../assets/icons/join-icon.svg";
 import { useState } from "react";
-import useGetUser from "../graphql/GetUser";
-import jwtDecode from "jwt-decode";
+import { useSelector } from "react-redux";
+import { Button } from "../components";
+import useUpdateAccount from "../graphql/UpdateAccount";
 
 const MyAccount = () => {
-  const [userId, setUserId] = useState('')
-  const getId = () =>{
-    const decoded = jwtDecode(localStorage.getItem("token"));
-    setUserId(decoded.userId);
-  }
-  useEffect(()=>{
-    getId();
-  }, [])
-  
-  const { data, loading, error } = useGetUser(userId);
+  const { insertAccountData, data, loading, error } = useUpdateAccount();
+  const [accountData, setAccountData] = useState({
+    fullName: "",
+    email: "",
+    telepon: "",
+  });
+  const { dataLogin } = useSelector((state) => state.login);
+
+  useEffect(() => {
+    setAccountData({
+      fullName: dataLogin?.fullName,
+      email: dataLogin?.email,
+      telepon: dataLogin?.telepon,
+    });
+  }, [dataLogin]);
+
   const [showSidebar, setShowSidebar] = useState(false);
+
   const handleSidebarShow = (e, show = !showSidebar) => setShowSidebar(show);
   const logout = () => alert("are u sure to logout ?");
   const dropdownItem = {
@@ -56,10 +64,27 @@ const MyAccount = () => {
       },
     ],
   };
- 
-  if (loading) return "Loading...";
+
+  const handleChange = (type, value) => {
+    const tempObj = {};
+    tempObj[type] = value;
+    let newObj = Object.assign({}, accountData, tempObj);
+    setAccountData(newObj);
+  };
+
+  const submitAccountChange = (e) => {
+    e.preventDefault();
+    insertAccountData({
+      variables: {
+        id: dataLogin.id,
+        fullName: accountData.fullName,
+        telepon: accountData.telepon,
+        email: accountData.email,
+      },
+    });
+  };
   if (error) return <pre>{error.message}</pre>;
-  console.log(data.user.findById)
+
   return (
     <>
       <Header usingToggle={true} toggleClick={handleSidebarShow}>
@@ -72,7 +97,7 @@ const MyAccount = () => {
               <img src="https://i.ibb.co/y0XWBqF/Ellipse-18.png" alt="avatar" />
             </div>
             <span className="text-2xl text-black font-medium mr-4">
-              Veronica
+              {dataLogin?.fullName}
             </span>
             <img src={ExpandIcon} alt="icon" className="w-[15px] h-[8px]" />
           </Dropdown>
@@ -81,15 +106,32 @@ const MyAccount = () => {
       <Sidebar isOpen={showSidebar} setIsOpen={handleSidebarShow} />
       <div className="flex justify-center mt-10">
         <div className="w-[85%] flex justify-between p-20 rounded-[20px] shadow-[4px_2px_8px_0px_rgba(0,0,0,0.25)]">
-          <form className="w-1/2">
+          <form className="w-1/2" onSubmit={submitAccountChange}>
             <h2 className="font-medium text-[32px] mb-10">My Account</h2>
-            <MyAccountInput inputFor="name" />
-            <MyAccountInput inputFor="email" />
-            <MyAccountInput inputFor="phone number" />
+            <MyAccountInput
+              inputFor="name"
+              value={accountData?.fullName}
+              handleChange={handleChange}
+            />
+            <MyAccountInput
+              inputFor="email"
+              value={accountData?.email}
+              handleChange={handleChange}
+            />
+            <MyAccountInput
+              inputFor="telepon"
+              value={accountData?.telepon}
+              handleChange={handleChange}
+            />
             <MyAccountInput inputFor="occupation" />
+            <Button
+              text="Change"
+              formBtn={true}
+              styling="py-4 mb-8 text-xl font-medium w-[100px] rounded-[10px]"
+            />
           </form>
           <div className="w-1/2 flex justify-center items-center">
-              <img src={require("../assets/img/myaccount.png")}/>
+            <img src={require("../assets/img/myaccount.png")} />
           </div>
         </div>
       </div>
