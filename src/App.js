96ebbdfde663d31e388b8e6b918986b2de4ee26c
Joese <fransasisi7@gmail.com>
2022-06-17 13:05:04 +0700

    fixing merge

--- src/App.js
+++ src/App.js
@@ -4,25 +4,20 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Landing from "./pages/Index";
 import Home from "./pages/Dashboard/home";
-<<<<<<< HEAD
 import Layout from "./components/Layout";
 import MyAccount from "./pages/MyAccount";
 import { useEffect, useState } from "react";
 import { setIsLoggedIn, setDecode, setDataLogin } from "./redux/loginSlice";
 import jwtDecode from "jwt-decode";
 import useGetUser from "./graphql/GetUser";
-=======
-import Layout from './components/Layout';
 import Classall from "./pages/Dashboard/classAll";
 import ClassStudent from "./pages/Dashboard/classStudent";
 import ClassTeacher from "./pages/Dashboard/classTeacher";
 
->>>>>>> 44f1bdca9c8ce57265cd75ed25075dce487e900c
-
 const App = () => {
   const dispatch = useDispatch();
   const { decode } = useSelector((state) => state.login);
-  const {dataLogin} = useSelector((state)=>state.login)
+  const { dataLogin } = useSelector((state) => state.login);
   const { data, loading } = useGetUser(decode);
 
   useEffect(() => {
@@ -32,7 +27,7 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    dispatch(setDataLogin(data?.user.findById))
+    dispatch(setDataLogin(data?.user.findById));
   }, [data]);
   return (
     <div>
@@ -40,20 +35,14 @@ const App = () => {
         <Route index element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Layout/>}>
-<<<<<<< HEAD
+        <Route path="/dashboard" element={<Layout />}>
           <Route path="home" element={<Home />} />
-=======
-       <Route path="/dashboard/class" element={<Classall />}/>
-       <Route path="/dashboard/student/class" element={<ClassStudent />} />
-       <Route path="/dashboard/teacher/class" element={<ClassTeacher />} />
-          <Route path='home' element={<Home/>}/>
->>>>>>> 44f1bdca9c8ce57265cd75ed25075dce487e900c
+          <Route path="/dashboard/class" element={<Classall />} />
+          <Route path="/dashboard/student/class" element={<ClassStudent />} />
+          <Route path="/dashboard/teacher/class" element={<ClassTeacher />} />
+          <Route path="home" element={<Home />} />
         </Route>
-        <Route
-          path="/myaccount"
-          element={<MyAccount/>}
-        />
+        <Route path="/myaccount" element={<MyAccount />} />
       </Routes>
     </div>
   );
