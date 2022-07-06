db210dacd2e0a7850093f6f6ed2ad3c845d7f355
Joese <fransasisi7@gmail.com>
2022-07-06 18:42:06 +0700

    add feature changeClassTeacher

--- src/components/Popup/ChangeClass.jsx
+++ src/components/Popup/ChangeClass.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 const ChangeClass = ({
+  key,
   title,
   code,
   url,
@@ -10,7 +11,7 @@ const ChangeClass = ({
     <>
       <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
         <td className="p-2 whitespace-nowrap">
-          <div className="flex items-center">
+          <div className="flex justify-center">
             <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
               <img
                 class="rounded-full"
@@ -20,15 +21,14 @@ const ChangeClass = ({
                 alt="Alex Shatov"
               />
             </div>
-            <div className="font-medium text-gray-800">{title}</div>
           </div>
         </td>
+        <td className="px-6 py-4">{title}</td>
         <td className="px-6 py-4">{code}</td>
-        <td className="px-6 py-4">1 July 2021 - 2 May 2022</td>
         <td className="px-6 py-4 text-right">
           <Link
             to={url}
-            className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+            className=" flex justify-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
           >
             Choose
           </Link>
