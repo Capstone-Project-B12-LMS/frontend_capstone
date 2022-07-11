0ee4711582549798c07585dc70e80e6cbdaf66dc
Joese <fransasisi7@gmail.com>
2022-07-11 10:37:45 +0700

    fixing clear darkmode

--- src/pages/Dashboard/History.jsx
+++ src/pages/Dashboard/History.jsx
@@ -30,7 +30,7 @@ const History = () => {
           <div className="container mx-auto px-24 mt-6">
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
               <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
                   <tr>
                     <th scope="col" colSpan="3" className="px-6 py-3 text-xl">
                       Today
@@ -45,24 +45,6 @@ const History = () => {
               </table>
             </div>
           </div>
-          {/* <div className="container mx-auto px-24 mt-6">
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                    <th scope="col" colSpan="3" className="px-6 py-3 text-xl">
-                      Yesterday
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <HistoryTable />
-
-                  <HistoryTable />
-                </tbody>
-              </table>
-            </div>
-          </div> */}
         </>
       )}
     </>
