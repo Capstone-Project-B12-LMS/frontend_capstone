387a3b8adb27573bcaf593a641a591fe914cab29
Joese <fransasisi7@gmail.com>
2022-07-11 10:30:32 +0700

    fixing clear darkmode

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -152,7 +152,7 @@ const Index = () => {
                             <input
                               type="text"
                               id="name-address-icon"
-                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  "
                               placeholder="Full Name"
                               name="name"
                               required
@@ -203,7 +203,7 @@ const Index = () => {
                             <textarea
                               type="text"
                               id="message-address-icon"
-                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  "
                               placeholder="Message"
                               rows="8"
                               cols="80"
